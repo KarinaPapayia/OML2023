2a95c063d541ed362e7e07d7a5e5b8714ff34ff2
pierre brechet <brechet@mis.mpg.de>
2023-04-19 16:31:49 +0200

    add comments

--- exercises/ex02/utils.py
+++ exercises/ex02/utils.py
@@ -3,7 +3,9 @@
 import numpy as np
 
 def gen_sin_data(N=100, sigma=0.1, range_x=(0,1)):
-    """Generate noisy sinusoidal 1D data"""
+    """Generate noisy sinusoidal 1D data
+
+    Output: (x,t) where x is of size N and t is of size N"""
     x = np.random.uniform(*range_x, (N,))  # N points uniformly distributed on range_x
     epsilon = sigma*np.random.randn(N)  # outputs a normal distribution that we scale with sigma
     t = np.sin(2*np.pi*x) + epsilon  # takes sin and add noise
@@ -21,7 +23,6 @@ def gen_binary_data(D=2, N=20):
     pts = np.random.uniform(-1,1, (N,D)) + T.reshape(1, -1)
     cls = np.sign(pts @ w + b)  # compute the class
     return (pts, cls)  # return the points and their classes
-    # pos = np.concatenate((np.ones(1, N//2), pos), axis=0)  # add the bias term inside the points
 
 
 
